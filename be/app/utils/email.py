a1afe32ad68e2fe0573b0a21eae60e0a3888c8a5
Tomas0512 <259459165+Tomas0512@users.noreply.github.com>
2026-09-08 18:13:09 -0500

    feat: add STARTTLS for SMTP and make SMTP config env-var driven [stash pop]

--- be/app/utils/email.py
+++ be/app/utils/email.py
@@ -43,8 +43,7 @@ def _send_email_smtp(to_email: str, subject: str, html: str) -> None:
     msg.attach(MIMEText(html, "html"))
 
     with smtplib.SMTP(settings.SMTP_HOST, settings.SMTP_PORT) as server:
-        if settings.SMTP_USE_TLS:
-            server.starttls()
+        server.starttls()
         if settings.SMTP_USERNAME:
             server.login(settings.SMTP_USERNAME, settings.SMTP_PASSWORD)
         server.send_message(msg)
